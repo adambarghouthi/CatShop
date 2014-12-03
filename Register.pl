10d17930e8652fa8d4492940b6a2ec0c5f89adc8
Adam Albarghouthi <adam.albarghouthi@mail.mcgill.ca>
2014-12-03 13:36:30 -0500

    minor

--- Register.pl
+++ Register.pl
@@ -2,3 +2,13 @@
 use strict;
 use CGI ':standard';
 
+$firstName = param('firstname');
+$lastName = param('lastname');
+$username = param('username');
+$password = param('password');
+
+sub main {
+	print "$firstName $lastName";
+}
+
+main();
