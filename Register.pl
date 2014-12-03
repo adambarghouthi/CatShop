b6f9cd4044b71b18adb7fedfed2d0f6fdec9f4bf
Adam Albarghouthi <adam.albarghouthi@mail.mcgill.ca>
2014-12-03 13:47:33 -0500

    minor

--- Register.pl
+++ Register.pl
@@ -7,8 +7,13 @@ $lastName = param('lastname');
 $username = param('username');
 $password = param('password');
 
-sub main {
-	print "$firstName $lastName";
-}
-
-main();
+print "Content-Type: text/html\n\n";
+print "<html>\n";
+print "<head>\n";
+print "<title> CGI/Perl Test </title>\n";
+print "</head>\n\n";
+print "<body>\n\n";
+print "Hello, world!<br><br>\n";
+print "Your name: $firstName $lastName<br><br>\n";
+print "</body>\n";
+print "</html>\n";
