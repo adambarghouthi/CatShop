ecb1b799839a8f5c92b9f518999ffcf7875ee68a
saghdaey <sanam.aghdaey@gmail.com>
2014-12-03 19:54:31 -0500

    isValid works
    isValid correctly checks if 2 strings form a valid username pwd
    combination found in Members.csv
    
    still need to: append username to LoggedIn.csv

--- login.c
+++ login.c
@@ -5,56 +5,47 @@
 #define DELIM ","
 
 
-int validPairOrNo(char[] username, char[] pwd)
+//method validPair returns 1 if username & password enterred are valid & 0 otherwise
+
+int validPair(char username[], char pwd[])
 {
 
 	FILE *members;
-	members=fopen("members.csv", "r");
+	members=fopen("Members.csv", "r");
 
 	char currLine[3000]; //represents 1 line of members.csv file
 	
 	while(fgets(currLine, sizeof(currLine), members))
 	{
 		char *token;
-		token=strtok(members, DELIM); //token set to 1st element of the current line of members
+		token=strtok(currLine, DELIM); //token set to 1st element of the current line of members
 		token=strtok(NULL, DELIM); //token set to 2nd element of currentl ine, so token=username listed in members.csv
 
-		if(stringcmp(username,token)==0) //this means the username matched 1 from Members.csv
-		{  //now check is password matched
-			//if password didn't match: break to return error page
-			//if we find password return 1
-			token=strtok(NULL,DELIM); //now token is the password of the current line
+		if(strcmp(username,token)==0) //check if valid username, if valid check password:
+		{  
+			token=strtok(NULL,DELIM); //make token password stored at current line (3rd entry of currLine)
 			char s[2]="\n";
 			pwdMembers=strtok(token, s); //to remove the \n from the last word in the current line
-			//or pwdMembers=strtok(token, '\n');
-			if(stringcmp(pwd, pwdMembers)==0)
+			if(stringcmp(pwd, pwdMembers)==0) //check if user enterred right password
 			{
 				return 1; //user logged in correctly
 			}
-			
 		}
-		//if we never returned 1 or broke from while: then username they enterred wasn't found
-	
-	return 0;
 	}
-
+	return 0; //invalid username password combination enterred
 }
 
 //void main(int argc, char* argv[])
 int main()
 {
-	///in MAIN METHOD
-	//initialize variables to textbox inputs 
-	//C algorithm: takes username & compare it to everything in members.csv 
-	//if that is valid- 
-	//check is password is valid 
-	//LIIMIT username to less than 50 characters
-	//use malloc to initialize these 
-	
-	char usernameInputted[50]="saghda";
-	char passwordMembers[50]="hello";
 
-	int x=validPairOrNo(usernameInputted, passwordinputted);
+	//initialize variables to textbox inputs 
+	//use malloc to initialize these 
+
+	char usernameInputted[50]="saghda";
+	char pwdMembers[50]="hello";
+
+	int x=validPair(usernameInputted, pwdInputted);
 	if(x==1)
 	{
 		printf("Logged In User \n");
